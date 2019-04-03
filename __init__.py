d31047785e0bc0b81ea026b315a1c9eb6ce0ecaa
Andrey Sorokin <andrey.sorokin@eosda.com>
2019-04-03 10:15:58 +0300

    add project to Pypi

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .builder.tile_creator import VisionBaseTileProcessor
-from .builder.contrib.model_creator import TableManager
-
-__all__ = [
-    VisionBaseTileProcessor,
-    TableManager
-]
--- setup.py
+++ setup.py
@@ -6,12 +6,17 @@ except ImportError:
 setup(
     name='vision_tile_query',
     version='0.0.1',
-    description='Library for create mVT SQL query',
-    long_description=open('README.md').read(),
+    description='Library for creating MVT SQL query',
+    long_description=open('Readme.md').read(),
     author='Andrey Sorokin',
     author_email='andreysrkn@eosda.com',
     url='https://github.com/eos-vision/vision_tile_query',
-    packages=['vision_tile_query'],
+    packages=['vision_tile_query',
+              'vision_tile_query/utils',
+              'vision_tile_query/contrib'],
+    install_requires=[
+        'SQLAlchemy', 'mercantile', 'geoalchemy2'
+    ],
     license='Unlicense',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -22,5 +27,4 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Database',
     ],
-    # TODO package_data
 )
