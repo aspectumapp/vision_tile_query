3e781157a9fadaf2271300d7d9937008452a53c5
Andrey Sorokin <andrey.sorokin@eosda.com>
2019-05-12 18:22:59 +0300

    create init file for test SQL query with docker db continer

---
+++ tests/test_sql_query.py
@@ -0,0 +1,18 @@
+def test_easy(db_session):
+    assert 1 == 1
+
+
+def test_db_query_1(db_session):
+    assert 1 == 1
+
+
+def test_db_query_2(db_session):
+    assert 1 == 1
+
+
+def test_db_query_3(db_session):
+    assert 1 == 1
+
+
+def test_db_query_4(db_session):
+    assert 1 == 1
